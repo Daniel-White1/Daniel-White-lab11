65e7a644dadcad6a10b7ccb8ffd465f3e1601cf1
Daniel White <daniel.white1@gwmail.gwu.edu>
2025-11-08 16:21:19 -0500

    Removed the error that test threads arent cleared after completion and finished test 3

--- Lab11_Tests.java
+++ Lab11_Tests.java
@@ -24,20 +24,17 @@ public class Lab11_Tests {
         } catch (Exception e){
             System.out.println("Error occured");
         }
-        ArrayList<String> sum = threadA.getData();
-
-        assertEquals(200, sum.size());
+        assertEquals(200, threadA.getData().size());
         System.out.println("Target Value is: 200");
-        System.out.println("Array list size is: " + sum.size());
+        System.out.println("Array list size is: " + threadA.getData().size());
         System.out.println("Lab11: Test 1 works.");
-
+        threadA.getData().clear();;
     }
     /*
         Complete the test case below that checks to see if the shared ArrayList has at least 10 entries after 500ms of system time
     */
     @Test
     public void test2() {
-
         Lab11_Thread threadA = new Lab11_Thread("A2", 500);
         Lab11_Thread threadB = new Lab11_Thread("B2", 500);
 
@@ -49,17 +46,24 @@ public class Lab11_Tests {
         } catch (Exception e){
             e.printStackTrace();
         }
-        ArrayList<String> sum = threadA.getData();
-        System.out.println("Shared Array List Size: " + sum.size());
-
+        System.out.println("Total Data Size: " + threadA.getData().size());
         boolean checker = false;
-        if (sum.size() > 10) {
+        if (threadA.getData().size() > 10) {
             checker = true;
         }
         assertEquals(true, checker);
         System.out.println("Test 2 is sucessful");
+        //Prevents the threads from overlapping into test 3
+        try{
+            threadA.join();
+            threadB.join();
+        } catch (Exception e) {
+            System.out.println("error occured");
+        }
+        System.out.println("Test 2 threads are over");
+        threadA.getData().clear();
     }
-    
+
     /*
         Complete the test case below that checks to see if thread A finishes adding its 10 entries before thread B was allowed to 
         add anything to the shared ArrayList
@@ -76,7 +80,17 @@ public class Lab11_Tests {
         } catch (Exception e){
             e.printStackTrace();
         }
-        
+        System.out.println("Thread size is: " + threadA.getData().size());
+        assertEquals(10, threadA.getData().size());
+        System.out.println("Test3 is correct");
         threadB.start();
+        try {
+            threadB.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //trying to compact if statements so I dont have to create new variables to track it.
+        assertEquals(false, (threadB.getData().size() == 10));
     }
 }
